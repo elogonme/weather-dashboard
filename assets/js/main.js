f46c264c02d4b5cf8bd53f4e312e4b446006d0cd
Eldar <elogon@gmail.com>
2020-11-06 20:37:19 -0500

    Describe app stories and pseudo code

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// What is the theme of the app?
+// to get weather data and 5 day forcast of the city which was entered from user.
+// When app starts I see Weather Dashboard.
+// On the left input field and search button to enter and search city.
+// Below input there is a history of search - clickable list of cities.
+// On the right side the weather card for the city which was clicked or searched which shows the weather info now.
+//  Below weather now card there is another 5 cards with 5 day forecast for that city.
+
+// When user enters city name and then search icon is clicked the request is getting sent to get weather.
+// Then Current weather and 5 day forecast is displayed.
+// The City name, the date, icon for current weather, temperature, humidity, wind speed, UV Index.
+// When UV index is dipsplayed the color is according to conditions - favorable, moderate, or severe.
+// The 5 day forecast shows datem icon for weather, temperature, and humidity.
+// Search history is shown and when user clicks on city name in search the relevant weather data is shown.
