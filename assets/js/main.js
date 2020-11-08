00bf4132ccad5ffccf41e4d1e572ab501596b237
Eldar <elogon@gmail.com>
2020-11-08 06:52:23 -0500

    Implement fetch data for weather uv and forecast, display weather now

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,14 @@
 $(document).ready(function(){
 
 // Initialize app.
-
+var city = {
+    name: '',
+    date: '',
+    temp: '',
+    humidity: '',
+    wind: '',
+    uv: ''
+}
 $('#search').on('click', function(event){
     event.preventDefault();
     $('#error').fadeOut();
@@ -24,13 +31,13 @@ $('#search').on('click', function(event){
     getCityWeather(city);
     
 });
-
-function getCityWeather(city){
-    var dateToday = moment().format('dddd, MMMM Do, YYYY'); // get todays date using moment.js
+// Function to fetch city weather now
+function getCityWeather(cityToGet){
+    var dateToday = moment().format('DD-MM-YYYY'); // get todays date using moment.js
     var hourNow = moment().format('H'); // get hour now using moment.js
     var apiKey = 'ff0f76a4c4d020f2b161d15988971e11';
-    var endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; // form API endpoint URL
-    fetch(endpoint).then(function(response){
+    var endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${cityToGet}&units=metric&appid=${apiKey}`; // form API endpoint URL
+    fetch(endpoint).then(function(response){    // Get City weather now info
         if (response.ok){
             return response.json();
         }
@@ -38,10 +45,62 @@ function getCityWeather(city){
         throw new Error('Request failed');
     }).then(function(data){
         console.log(data);
-    }).catch(function(err){
+        city.name = data.name;
+        city.lat = data.coord.lat;
+        city.lon = data.coord.lon;
+        city.date = dateToday;
+        city.temp = data.main.temp;
+        city.humidity = data.main.humidity;
+        city.wind =  data.wind.speed;
+        city.icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        console.log(city);
+        return city;
+    }).then(function(city){
+        // Get City Uv Index
+        var endpointUv = `http://api.openweathermap.org/data/2.5/uvi?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`;
+        fetch(endpointUv).then(function(response){
+            if (response.ok){
+                return response.json();
+            }
+        }).then(function(data){
+            city.uv = data.value;
+        })
+     
+    }).then(function(){
+        // Get 5-day forecast
+        var endpointForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${cityToGet}&units=metric&appid=${apiKey}`; 
+        
+        fetch(endpointForecast).then(function(response){
+            if (response.ok){
+                return response.json();
+            }
+        }).then(function(data){
+            city.forecast = data.list;
+            displayWeather(city)
+        })
+    })
+        
+    .catch(function(err){
         console.log(err);
     })
 };
+
+// Function to fetch City UV Index
+// Function to fetch City 5-day forecast
+// function to display retreived weather now on the page
+function displayWeather(city) {
+    console.log(city);
+    $('#city-name').text(city.name);
+    $('#date').text(city.date);
+    $('#icon').attr('src', city.icon);
+    $('#temp').text(city.temp);
+    $('#humidity').text(city.humidity);
+    $('#wind').text(city.wind);
+    $('#uv').text(city.uv);
+
+}
+
+
 
 });
 // 1. When user enters city name and then search icon is clicked the request is getting sent to get weather.
